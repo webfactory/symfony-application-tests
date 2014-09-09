6dc644534bf821c2e75330812c7267805a0c0ee1
Matthias Molitor <mmo@webfactory.de>
2014-09-09 16:56:30 +0200

    added base class for container tests

---
+++ src/Tests/Integration/AbstractContainerTestCase.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Integration;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Base class for integration test cases.
+ *
+ * Provides access to kernel and container.
+ */
+abstract class AbstractContainerTestCase extends WebTestCase
+{
+
+    /**
+     * Returns the annotation reader that is used by the application.
+     *
+     * @return \Doctrine\Common\Annotations\Reader
+     */
+    protected function getAnnotationReader()
+    {
+        return $this->getContainer()->get('annotation_reader');
+    }
+
+    /**
+     * Returns the container.
+     *
+     * @return \Symfony\Component\DependencyInjection\Container
+     */
+    protected function getContainer()
+    {
+        $container = static::createClient()->getContainer();
+        $message   = 'We need a container instance to inspect the services; '
+                   . 'a class that implements the ContainerInterface is not enough.';
+        $this->assertInstanceOf('\Symfony\Component\DependencyInjection\Container', $container, $message);
+        return $container;
+    }
+
+}
