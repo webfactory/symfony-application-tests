67e15cd25639da13140eb2951494fd390c8372ae
Matthias Molitor <mmo@webfactory.de>
2014-09-16 15:10:15 +0200

    handled special cases if router is missing or if no non-service controllers are available

---
+++ src/Webfactory/Tests/Integration/ControllerTest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Webfactory\Tests\Integration;
+
+use Doctrine\Common\Util\ClassUtils;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Tests general configuration of the controller classes.
+ */
+class ControllerTest extends AbstractContainerTestCase
+{
+
+    /**
+     * Ensures that controllers, which are not defined as service, do not use
+     * "secure" annotations (these would be ignored).
+     *
+     * @param string $class The name of the controller class.
+     * @dataProvider nonServiceControllerClassNameProvider
+     */
+    public function testNonServiceControllerDoesNotUseSecureAnnotations($class = null)
+    {
+        if ($class === null) {
+            $this->markTestSkipped('No controllers found that are not registered as service. Nothing to test.');
+        }
+        $info = new \ReflectionClass($class);
+        $methodsWithAnnotation = array();
+        foreach ($info->getMethods() as $method) {
+            /* @var $method \ReflectionMethod */
+            /* @var $annotation \JMS\SecurityExtraBundle\Annotation\Secure */
+            $annotation = $this->getAnnotationReader()->getMethodAnnotation(
+                $method,
+                '\JMS\SecurityExtraBundle\Annotation\Secure'
+            );
+            if ($annotation !== null) {
+                $methodsWithAnnotation[] = $method->getName();
+            }
+        }
+        $message = 'Controller %s is not defined as a service, '
+                 . 'but the following methods use @Secure annotations. ' .PHP_EOL
+                 . 'These annotations work only for services:' . PHP_EOL
+                 . implode(PHP_EOL, $methodsWithAnnotation);
+        $message = sprintf($message, $class);
+        $this->assertCount(0, $methodsWithAnnotation, $message);
+    }
+
+    /**
+     * Returns the class names of all controllers that are not defined as a service.
+     *
+     * @return array(array(string))
+     */
+    public function nonServiceControllerClassNameProvider()
+    {
+        $classes = array();
+        foreach ($this->getControllerDefinitions() as $definition) {
+            /* @var $definition \stdClass */
+            if ($definition->isService) {
+                continue;
+            }
+            $classes[] = $definition->class;
+        }
+        if (count($classes) === 0) {
+            // Return a dummy entry to avoid a failing test.
+            return array(array());
+        }
+        $classes = array_unique($classes);
+        return array_map(function ($class) {
+            return array($class);
+        }, $classes);
+    }
+
+    /**
+     * Returns the annotation reader that is used by the application.
+     *
+     * @return \Doctrine\Common\Annotations\Reader
+     */
+    protected function getAnnotationReader()
+    {
+        return $this->getContainer()->get('annotation_reader');
+    }
+
+    /**
+     * Returns information about controllers that are used in the application.
+     *
+     * The data entries are simple objects that provide the following attributes:
+     *
+     * - isService (boolean) - Determines if the controller is configured as service.
+     * - class (string)      - The (original) class name of the controller.
+     * - controller (object) - The instantiated controller.
+     *
+     * @return array(object)
+     */
+    protected function getControllerDefinitions()
+    {
+        if (!$this->getContainer()->has('router')) {
+            return array();
+        }
+        $routes      = $this->getContainer()->get('router')->getRouteCollection()->all();
+        $definitions = array();
+        foreach ($routes as $route) {
+            /* @var $route \Symfony\Component\Routing\Route */
+            $assignedController = $route->getDefault('_controller');
+            if ($assignedController === null) {
+                // No controller is assigned to this route.
+                continue;
+            }
+            if (strpos($assignedController, 'assetic.') === 0) {
+                // Ignore Assetic controllers.
+                continue;
+            }
+            $controller = $this->createController($assignedController);
+            if ($controller === null) {
+                continue;
+            }
+            $definition = new \stdClass();
+            $definition->isService  = substr_count($assignedController, ':') === 1;
+            $definition->class      = $this->toClassName($controller);
+            $definition->controller = $controller;
+            $definitions[] = $definition;
+        }
+        return $definitions;
+    }
+
+    /**
+     * Uses the provided controller to determine the original controller class name.
+     *
+     * Controllers might be encapsulated by proxies, which requires a special treatment.
+     *
+     * @param object $controller
+     * @return string
+     */
+    protected function toClassName($controller)
+    {
+        return ClassUtils::getClass($controller);
+    }
+
+    /**
+     * Uses the route reference to create an controller object.
+     *
+     * @param mixed $reference The controller reference from a route.
+     * @return object|null
+     */
+    protected function createController($reference)
+    {
+        /* @var $resolver \Symfony\Component\HttpKernel\Controller\ControllerResolverInterface */
+        $resolver         = $this->getContainer()->get('debug.controller_resolver');
+        $simulatedRequest = new Request(array(), array(), array('_controller' => $reference));
+        $controller       = $resolver->getController($simulatedRequest);
+        if (is_array($controller) && is_object($controller[0])) {
+            $controller = $controller[0];
+        }
+        if (!is_object($controller)) {
+            // Controller might be callable, which is not of interest for us.
+            return null;
+        }
+        return $controller;
+    }
+
+}
---
+++ src/Webfactory/Tests/Integration/TwigTemplateTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Webfactory\Tests\Integration;
+
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+/**
+ * Checks the Twig templates in the project.
+ */
+class TwigTemplateTest extends AbstractContainerTestCase
+{
+
+    /**
+     * Checks if the provided Twig templates can be compiled.
+     *
+     * @param string $templatePath The path to the template file.
+     * @dataProvider templateFileProvider
+     */
+    public function testTemplateCanBeCompiled($templatePath)
+    {
+        $loader = new \Twig_Loader_Filesystem(dirname($templatePath));
+        $twig   = $this->getTwigEnvironment();
+        // Add the new loader to be able to load the template directly.
+        // The original loader must be preserved as Twig is otherwise
+        // not able to resolve the inline references.
+        $combinedLoader = new \Twig_Loader_Chain(array($loader, $twig->getLoader()));
+        $twig->setLoader($combinedLoader);
+        $fileName = basename($templatePath);
+
+        $this->setExpectedException(null);
+        $twig->loadTemplate($fileName);
+    }
+
+    /**
+     * Provider that can be used by tests to retrieve the template file paths.
+     *
+     * @return array(array(string))
+     */
+    public function templateFileProvider()
+    {
+        return array_map(function ($path) {
+            return array($path);
+        }, $this->getTemplateFiles());
+    }
+
+    /**
+     * Returns the Twig environment that is used by the application.
+     *
+     * The application specific environment must be used, as the default
+     * one does not know about Symfony or custom extensions.
+     *
+     * @return \Twig_Environment
+     */
+    protected function getTwigEnvironment()
+    {
+        $environment = $this->getContainer()->get('twig', Container::NULL_ON_INVALID_REFERENCE);
+        if (!($environment instanceof \Twig_Environment)) {
+            $this->markTestSkipped('Twig is not enabled for this application.');
+        }
+        // Return a copy to ensure that the original service configuration is preserved.
+        return clone $environment;
+    }
+
+    /**
+     * Returns the paths to the template files in this project.
+     *
+     * @return array(string)
+     */
+    protected function getTemplateFiles()
+    {
+        $templates = Finder::create()->in(__DIR__ . '/../../src')->files()->name('*.*.twig');
+        $templates = iterator_to_array($templates, false);
+        $templates = array_map(function (SplFileInfo $file) {
+            return $file->getRealPath();
+        }, $templates);
+        return $templates;
+    }
+
+}
