ac116d01a45ee5aee4a5126a1c8f3a1558419ec2
Matthias Molitor <mmo@webfactory.de>
2014-09-16 13:51:50 +0200

    added minimal TestKernel

---
+++ tests/_files/test-application/TestKernel.php
@@ -0,0 +1,35 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * Minimal Kernel that is used for testing.
+ */
+class TestKernel extends Kernel
+{
+    /**
+     * Returns an array of bundles to register.
+     *
+     * @return \Symfony\Component\HttpKernel\Bundle\BundleInterface[] An array of bundle instances.
+     * @api
+     */
+    public function registerBundles()
+    {
+        return array(
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle()
+        );
+    }
+
+    /**
+     * Loads the container configuration.
+     *
+     * @param LoaderInterface $loader A LoaderInterface instance
+     * @api
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+}
