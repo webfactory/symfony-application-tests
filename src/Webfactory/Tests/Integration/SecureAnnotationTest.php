5857ff8ddecf9a08c4915ee03f94f8c4ece0f326
Matthias Molitor <mmo@webfactory.de>
2014-09-24 14:27:36 +0200

    moved controller test as it is related to the @Secure annotation

--- src/Webfactory/Tests/Integration/SecureAnnotationTest.php
+++ src/Webfactory/Tests/Integration/SecureAnnotationTest.php
@@ -2,7 +2,9 @@
 
 namespace Webfactory\Tests\Integration;
 
+use Doctrine\Common\Util\ClassUtils;
 use JMS\SecurityExtraBundle\Annotation\Secure;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Tests the @Secure annotations that are used in the application.
@@ -38,6 +40,39 @@ class SecureAnnotationTest extends AbstractContainerTestCase
             );
             $this->assertContains($role, $existingRoles, $message);
         }
+    }
+
+    /**
+     * Ensures that controllers, which are not defined as service, do not use
+     * "secure" annotations (these would be ignored).
+     *
+     * @param string $class The name of the controller class.
+     * @dataProvider nonServiceControllerClassNameProvider
+     */
+    public function testNonServiceControllerDoesNotUseSecureAnnotations($class = null)
+    {
+        if ($class === null) {
+            $this->markTestSkipped('No controllers found that are not registered as service. Nothing to test.');
+        }
+        $info = new \ReflectionClass($class);
+        $methodsWithAnnotation = array();
+        foreach ($info->getMethods() as $method) {
+            /* @var $method \ReflectionMethod */
+            /* @var $annotation \JMS\SecurityExtraBundle\Annotation\Secure */
+            $annotation = $this->getAnnotationReader()->getMethodAnnotation(
+                $method,
+                '\JMS\SecurityExtraBundle\Annotation\Secure'
+            );
+            if ($annotation !== null) {
+                $methodsWithAnnotation[] = $method->getName();
+            }
+        }
+        $message = 'Controller %s is not defined as a service, '
+            . 'but the following methods use @Secure annotations. ' .PHP_EOL
+            . 'These annotations work only for services:' . PHP_EOL
+            . implode(PHP_EOL, $methodsWithAnnotation);
+        $message = sprintf($message, $class);
+        $this->assertCount(0, $methodsWithAnnotation, $message);
     }
 
     /**
@@ -101,5 +136,104 @@ class SecureAnnotationTest extends AbstractContainerTestCase
             $roles = array_merge($roles, $inheritedRoles);
         }
         return array_unique($roles);
+    }
+
+    /**
+     * Returns the class names of all controllers that are not defined as a service.
+     *
+     * @return array(array(string))
+     */
+    public function nonServiceControllerClassNameProvider()
+    {
+        $classes = array();
+        foreach ($this->getControllerDefinitions() as $definition) {
+            /* @var $definition \stdClass */
+            if ($definition->isService) {
+                continue;
+            }
+            $classes[] = $definition->class;
+        }
+        $classes = array_unique($classes);
+        $data    = array_map(function ($class) {
+            return array($class);
+        }, $classes);
+        return $this->addFallbackEntryToProviderDataIfNecessary($data);
+    }
+
+    /**
+     * Returns information about controllers that are used in the application.
+     *
+     * The data entries are simple objects that provide the following attributes:
+     *
+     * - isService (boolean) - Determines if the controller is configured as service.
+     * - class (string)      - The (original) class name of the controller.
+     * - controller (object) - The instantiated controller.
+     *
+     * @return array(object)
+     */
+    protected function getControllerDefinitions()
+    {
+        if (!$this->getContainer()->has('router')) {
+            return array();
+        }
+        $routes      = $this->getContainer()->get('router')->getRouteCollection()->all();
+        $definitions = array();
+        foreach ($routes as $route) {
+            /* @var $route \Symfony\Component\Routing\Route */
+            $assignedController = $route->getDefault('_controller');
+            if ($assignedController === null) {
+                // No controller is assigned to this route.
+                continue;
+            }
+            if (strpos($assignedController, 'assetic.') === 0) {
+                // Ignore Assetic controllers.
+                continue;
+            }
+            $controller = $this->createController($assignedController);
+            if ($controller === null) {
+                continue;
+            }
+            $definition = new \stdClass();
+            $definition->isService  = substr_count($assignedController, ':') === 1;
+            $definition->class      = $this->toClassName($controller);
+            $definition->controller = $controller;
+            $definitions[] = $definition;
+        }
+        return $definitions;
+    }
+
+    /**
+     * Uses the provided controller to determine the original controller class name.
+     *
+     * Controllers might be encapsulated by proxies, which requires a special treatment.
+     *
+     * @param object $controller
+     * @return string
+     */
+    protected function toClassName($controller)
+    {
+        return ClassUtils::getClass($controller);
+    }
+
+    /**
+     * Uses the route reference to create an controller object.
+     *
+     * @param mixed $reference The controller reference from a route.
+     * @return object|null
+     */
+    protected function createController($reference)
+    {
+        /* @var $resolver \Symfony\Component\HttpKernel\Controller\ControllerResolverInterface */
+        $resolver         = $this->getContainer()->get('debug.controller_resolver');
+        $simulatedRequest = new Request(array(), array(), array('_controller' => $reference));
+        $controller       = $resolver->getController($simulatedRequest);
+        if (is_array($controller) && is_object($controller[0])) {
+            $controller = $controller[0];
+        }
+        if (!is_object($controller)) {
+            // Controller might be callable, which is not of interest for us.
+            return null;
+        }
+        return $controller;
     }
 }
