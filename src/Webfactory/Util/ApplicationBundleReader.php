a562517e6ac4884d56f4a334e47f3226313e5ad3
Matthias Molitor <mmo@webfactory.de>
2014-10-06 10:29:24 +0200

    added stub for reader that returns only application bundles (#3)

---
+++ src/Webfactory/Util/ApplicationBundleReader.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Webfactory\Util;
+
+use Traversable;
+
+/**
+ * Allows iteration over bundles that are defined in the current application.
+ *
+ * External bundles (which are included from the vendor directory) are not included.
+ */
+class ApplicationBundleReader implements \IteratorAggregate
+{
+    /**
+     * Returns the iterator.
+     *
+     * @return Traversable
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     */
+    public function getIterator()
+    {
+        // TODO: Implement getIterator() method.
+    }
+}
