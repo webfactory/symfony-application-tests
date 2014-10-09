613f1e34bdf1cb83c1a21080b29339ea2651fd64
Matthias Molitor <mmo@webfactory.de>
2014-10-09 23:50:07 +0200

    added comments and test stub (#3)

--- tests/Webfactory/Util/VendorResourcesTest.php
+++ tests/Webfactory/Util/VendorResourcesTest.php
@@ -27,26 +27,55 @@ class VendorResourcesTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(realpath($vendorDirectory), realpath(VendorResources::getVendorDirectory()));
     }
 
+    /**
+     * Ensures that isVendorClass() returns false if the source file of the given class
+     * is not located in the vendor directory.
+     */
     public function testIsVendorClassReturnsFalseIfClassIsNotLocatedInVendorDirectory()
     {
 
     }
 
+    /**
+     * Ensures that isVendorClass() returns false if the name of an internal PHP class
+     * is passed.
+     */
+    public function testIsVendorClassReturnsFalseIfNameOfInternalClassIsPassed()
+    {
+
+    }
+
+    /**
+     * Ensures that isVendorClass() returns true if the name of a class from the vendor
+     * directory is passed.
+     */
     public function testIsVendorClassReturnsTrueIfClassIsLocatedInVendorDirectory()
     {
 
     }
 
+    /**
+     * Ensures that isVendorClass() returns false if an object is passed whose class
+     * is not located in the vendor directory.
+     */
     public function testIsVendorClassReturnsFalseIfClassOfObjectIsNotLocatedInVendorDirectory()
     {
 
     }
 
+    /**
+     * Ensures that isVendorClass() returns true if an object is passed whose class
+     * is located in the vendor directory.
+     */
     public function testIsVendorClassReturnsTrueIfClassOfObjectIsLocatedInVendorDirectory()
     {
 
     }
 
+    /**
+     * Ensures that isVendorClass() throws an exception if neither an object nor a valid
+     * class name is passed.
+     */
     public function testIsVendorClassThrowsExceptionIfNoClassNameOrObjectIsPassed()
     {
 
