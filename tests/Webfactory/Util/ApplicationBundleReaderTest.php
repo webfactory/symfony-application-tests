8cee3ae83897951006770b6bd14e254d96e17203
Matthias Molitor <mmo@webfactory.de>
2014-10-09 23:06:29 +0200

    added test initialization and comments (#3)

--- tests/Webfactory/Util/ApplicationBundleReaderTest.php
+++ tests/Webfactory/Util/ApplicationBundleReaderTest.php
@@ -8,12 +8,19 @@ namespace Webfactory\Util;
 class ApplicationBundleReaderTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * System under test.
+     *
+     * @var \Webfactory\Util\ApplicationBundleReader
+     */
+    protected $reader = null;
+
+    /**
      * Initializes the test environment.
      */
     protected function setUp()
     {
         parent::setUp();
-
+        $this->reader = new ApplicationBundleReader(new \TestKernel('test', true));
     }
 
     /**
@@ -21,25 +28,38 @@ class ApplicationBundleReaderTest extends \PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
-
+        $this->reader = null;
         parent::tearDown();
     }
 
+    /**
+     * Checks if the reader is traversable.
+     */
     public function testReaderIsTraversable()
     {
 
     }
 
+    /**
+     * Checks if the reader iterates over bundle instances.
+     */
     public function testReaderIteratesOverBundles()
     {
 
     }
 
+    /**
+     * Ensures that the reader returns bundles that are defined in the application.
+     */
     public function testReaderReturnsApplicationBundles()
     {
 
     }
 
+    /**
+     * Ensures that bundles, which are active in the application, but defined in the vendor
+     * directory, are not returned by the reader.
+     */
     public function testReaderDoesNotReturnActiveVendorBundles()
     {
 
