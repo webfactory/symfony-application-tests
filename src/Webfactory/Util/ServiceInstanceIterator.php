4782bbf5bd5856e1f89fd1525559dbc0f7458e1b
Matthias Molitor <mmo@webfactory.de>
2014-10-10 16:30:31 +0200

    added iterator stub (#3)

---
+++ src/Webfactory/Util/ServiceInstanceIterator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Accepts a set of service IDs and allows the iteration over the corresponding
+ * instantiated services.
+ *
+ * Skips services are synthetic or which depend on synthetic services.
+ */
+class ServiceInstanceIterator
+{
+
+}
