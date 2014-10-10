e549d18930282b20edd726014f9c2d33a13c7fc2
Matthias Molitor <mmo@webfactory.de>
2014-10-10 16:32:09 +0200

    added iterator stub (#3)

---
+++ src/Webfactory/Util/DataProviderIterator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Helper class that simplifies the usage of data providers in tests.
+ *
+ * Automatically adds a null entries if the provided data set is empty.
+ * The test that handles the provider data must be able to handle that case (0 arguments).
+ */
+class DataProviderIterator
+{
+
+}
