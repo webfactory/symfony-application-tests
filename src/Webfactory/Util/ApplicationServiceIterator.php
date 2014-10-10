e852351ab4ca745f5798efd96d9ce07640d6a0e3
Matthias Molitor <mmo@webfactory.de>
2014-10-10 16:27:14 +0200

    improved class stub (#3)

--- src/Webfactory/Util/ApplicationServiceIterator.php
+++ src/Webfactory/Util/ApplicationServiceIterator.php
@@ -2,6 +2,9 @@
 
 namespace Webfactory\Util;
 
+use Symfony\Component\HttpKernel\KernelInterface;
+use Traversable;
+
 /**
  * Allows iteration over service that are defined directly in the application.
  *
@@ -13,8 +16,31 @@ namespace Webfactory\Util;
  * - Try to determine, which services are defined by the extensions of the application bundles and
  *   whitelist these.
  * - Follow conventions and allow services that use the prefixes of the application bundle extensions.
+ *
+ * The iterator operates on a given list of service IDs and provides only the IDs of the services
+ * that are defined in the application.
  */
-class ApplicationServiceIterator
+class ApplicationServiceIterator implements \IteratorAggregate
 {
+    /**
+     * Creates the iterator.
+     *
+     * @param KernelInterface $applicationKernel
+     * @param \Traversable $serviceIds The service IDs that are filtered.
+     */
+    public function __construct(KernelInterface $applicationKernel, \Traversable $serviceIds)
+    {
 
+    }
+
+    /**
+     * Returns the iterator.
+     *
+     * @return \Traversable
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     */
+    public function getIterator()
+    {
+
+    }
 }
