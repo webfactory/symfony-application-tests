819c87429e415bf4e6a65bf4cb95a0dd3d89180c
Matthias Molitor <mmo@webfactory.de>
2014-10-10 16:38:21 +0200

    added test stubs (#3)

---
+++ tests/Webfactory/Util/ApplicationServiceIteratorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Tests the application service iterator.
+ */
+class ApplicationServiceIteratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsTraversable()
+    {
+
+    }
+
+    public function testProvidesServiceThatIsDefinedInApplicationBundleButDoesNotFollowConvention()
+    {
+
+    }
+
+    public function testProvidesServiceThatIsDefinedInApplicationBundleAndFollowsConvention()
+    {
+
+    }
+
+    public function testSkipsServiceFromVendorBundle()
+    {
+
+    }
+}
