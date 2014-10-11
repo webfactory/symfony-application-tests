2fbd924631afa3f05941cb1f3da6a52ad8fb05d2
Matthias Molitor <mmo@webfactory.de>
2014-10-11 17:53:04 +0200

    implemented service creator (#3)

--- src/Webfactory/Util/ServiceCreator.php
+++ src/Webfactory/Util/ServiceCreator.php
@@ -2,7 +2,9 @@
 
 namespace Webfactory\Util;
 
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
 
 /**
  * Creates services from a container.
@@ -12,13 +14,20 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ServiceCreator
 {
     /**
+     * The container that is used to retrieve service instances.
+     *
+     * @var ContainerInterface
+     */
+    protected $container = null;
+
+    /**
      * Creates a factory for services from the given container.
      *
      * @param ContainerInterface $container
      */
     public function __construct(ContainerInterface $container)
     {
-
+        $this->container = $container;
     }
 
     /**
@@ -30,6 +39,29 @@ class ServiceCreator
      */
     public function create($serviceId)
     {
+        try {
+            return $this->container->get($serviceId, Container::NULL_ON_INVALID_REFERENCE);
+        } catch (\Exception $e) {
+            if ($this->isCausedBySyntheticServiceRequest($e)) {
+                // Ignore errors that are caused by synthetic services. It is simply not
+                // possible to create them in a reliable way.
+                return null;
+            }
+            throw new CannotInstantiateServiceException($serviceId, 0, $e);
+        }
+    }
 
+    /**
+     * Checks if a request for a synthetic service caused the provided exception.
+     *
+     * @param \Exception $exception
+     * @return boolean
+     */
+    protected function isCausedBySyntheticServiceRequest(\Exception $exception)
+    {
+        if (!($exception instanceof RuntimeException)) {
+            return false;
+        }
+        return strpos($exception->getMessage(), 'requested a synthetic service') !== false;
     }
 }
