0fa6b3e5c2056ea2b8a103585a3bc325b00498b1
Matthias Molitor <mmo@webfactory.de>
2014-10-11 21:01:41 +0200

    added test case stub (#3)

---
+++ tests/Webfactory/Util/DataProviderIteratorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Tests the data provider iterator.
+ */
+class DataProviderIteratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsTraversable()
+    {
+
+    }
+
+    public function testAddsEntryIfDataSetIsEmpty()
+    {
+
+    }
+
+    public function testDoesNotChangeDataSetThatIsNotEmpty()
+    {
+
+    }
+}
