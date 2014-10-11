f43daa9fe37fa4fa1e2408df0f8e15084b28f0a6
Matthias Molitor <mmo@webfactory.de>
2014-10-11 21:33:33 +0200

    implemented iterator (#3)

--- src/Webfactory/Util/DataProviderIterator.php
+++ src/Webfactory/Util/DataProviderIterator.php
@@ -8,7 +8,36 @@ namespace Webfactory\Util;
  * Automatically adds a null entries if the provided data set is empty.
  * The test that handles the provider data must be able to handle that case (0 arguments).
  */
-class DataProviderIterator
+class DataProviderIterator implements \IteratorAggregate
 {
+    /**
+     * The data set.
+     *
+     * @var array(array(mixed))
+     */
+    protected $dataSet = null;
 
+    /**
+     * Creates an iterator that allows to traverse the given data set.
+     *
+     * @param array(array(mixed)) $dataSet
+     */
+    public function __construct(array $dataSet)
+    {
+        $this->dataSet = $dataSet;
+    }
+
+    /**
+     * Returns the iterator.
+     *
+     * @return \Traversable
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     */
+    public function getIterator()
+    {
+        if (count($this->dataSet) === 0) {
+            return new \ArrayIterator(array(array()));
+        }
+        return new \ArrayIterator($this->dataSet);
+    }
 }
