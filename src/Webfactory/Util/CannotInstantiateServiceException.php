f4f8e4eb59eb49837f57453f88b635c1609c6b97
Matthias Molitor <mmo@webfactory.de>
2014-10-11 20:37:28 +0200

    improved exception (#3)

--- src/Webfactory/Util/CannotInstantiateServiceException.php
+++ src/Webfactory/Util/CannotInstantiateServiceException.php
@@ -7,5 +7,18 @@ namespace Webfactory\Util;
  */
 class CannotInstantiateServiceException extends \RuntimeException
 {
-
+    /**
+     * Creates an exception, which indicates that it was not possible to create
+     * the service with the given ID.
+     *
+     *
+     * @param string $serviceId
+     * @param integer $code
+     * @param \Exception $previous The original creation failure.
+     */
+    public function __construct($serviceId, $code = 0, \Exception $previous = null)
+    {
+        $message = 'Cannot create service "' . $serviceId . '".';
+        parent::__construct($message, $code, $previous);
+    }
 }
