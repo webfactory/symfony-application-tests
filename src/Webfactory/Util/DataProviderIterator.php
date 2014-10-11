e5f0052c58d0bc52fcf1a8064e73b7cb17c5ff4f
Matthias Molitor <mmo@webfactory.de>
2014-10-11 21:53:36 +0200

    improved comment (#3)

--- src/Webfactory/Util/DataProviderIterator.php
+++ src/Webfactory/Util/DataProviderIterator.php
@@ -6,7 +6,13 @@ namespace Webfactory\Util;
  * Helper class that simplifies the usage of data providers in tests.
  *
  * Automatically adds a null entries if the provided data set is empty.
- * The test that handles the provider data must be able to handle that case (0 arguments).
+ *
+ * If a data provider returns an empty array, then the test fails. Therefore, this iterator adds an
+ * entry if no data was gathered by a provider.
+ * This entry does not provide any argument, so the test that works with the data should use
+ * only optional parameters and handle that special case (for example by skipping the test).
+ *
+ * @see https://phpunit.de/manual/3.9/en/writing-tests-for-phpunit.html#writing-tests-for-phpunit.data-providers
  */
 class DataProviderIterator implements \IteratorAggregate
 {
