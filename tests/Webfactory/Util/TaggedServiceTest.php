7508b2bf941e4a27a9abf1cc229bc1ae778e14ab
Matthias Molitor <mmo@webfactory.de>
2014-10-12 10:01:47 +0200

    added test case stub (#3)

---
+++ tests/Webfactory/Util/TaggedServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Tests the class that holds information about a tagged service.
+ */
+class TaggedServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Checks if getServiceId() returns the correct value.
+     */
+    public function testGetServiceIdReturnsCorrectValue()
+    {
+
+    }
+
+    /**
+     * Checks if getTagDefinition() returns the correct value.
+     */
+    public function getTagDefinitionReturnsCorrectValue()
+    {
+
+    }
+
+    /**
+     * Checks if __toString() returns the service ID.
+     */
+    public function testToStringReturnsServiceId()
+    {
+
+    }
+}
