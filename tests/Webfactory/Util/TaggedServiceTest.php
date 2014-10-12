1d73034d7114e32c0057803f5aa474828d62d048
Matthias Molitor <mmo@webfactory.de>
2014-10-12 11:34:16 +0200

    implemented tests (#3)

--- tests/Webfactory/Util/TaggedServiceTest.php
+++ tests/Webfactory/Util/TaggedServiceTest.php
@@ -8,19 +8,52 @@ namespace Webfactory\Util;
 class TaggedServiceTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * System under test.
+     *
+     * @var TaggedService
+     */
+    protected $taggedService = null;
+
+    /**
+     * Initializes the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->taggedService = new TaggedService(
+            'my.service',
+            array(
+                'alias' => 'test'
+            )
+        );
+    }
+
+    /**
+     * Cleans up the test environment.
+     */
+    protected function tearDown()
+    {
+        $this->taggedService = null;
+        parent::tearDown();
+    }
+
+    /**
      * Checks if getServiceId() returns the correct value.
      */
     public function testGetServiceIdReturnsCorrectValue()
     {
-
+        $this->assertEquals('my.service', $this->taggedService->getServiceId());
     }
 
     /**
      * Checks if getTagDefinition() returns the correct value.
      */
-    public function getTagDefinitionReturnsCorrectValue()
+    public function testGetTagDefinitionReturnsCorrectValue()
     {
-
+        $expected = array(
+            'alias' => 'test'
+        );
+        $this->assertEquals($expected, $this->taggedService->getTagDefinition());
     }
 
     /**
@@ -28,6 +61,6 @@ class TaggedServiceTest extends \PHPUnit_Framework_TestCase
      */
     public function testToStringReturnsServiceId()
     {
-
+        $this->assertEquals($this->taggedService->getServiceId(), (string)$this->taggedService);
     }
 }
