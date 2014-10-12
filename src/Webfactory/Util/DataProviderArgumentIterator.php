98f2b6da7e649bfc9ad78aaf9d3f9efd5169eadc
Matthias Molitor <mmo@webfactory.de>
2014-10-12 17:17:44 +0200

    added iterator and test case stub (#3)

---
+++ src/Webfactory/Util/DataProviderArgumentIterator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Iterator that converts a single dimensional list into argument lists
+ * for methods that use a data provider.
+ *
+ * A single argument entry is an array that contains the one element for
+ * each argument that is passed.
+ */
+class DataProviderArgumentIterator
+{
+
+}
---
+++ tests/Webfactory/Util/DataProviderArgumentIteratorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Tests the argument iterator.
+ */
+class DataProviderArgumentIteratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsTraversable()
+    {
+
+    }
+
+    public function testIteratorDoesNotChangeNumberOfEntries()
+    {
+
+    }
+
+    public function testIteratorConvertsOriginalEntriesToArgumentSets()
+    {
+
+    }
+}
