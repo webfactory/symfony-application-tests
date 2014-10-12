1c4ee338097229fe77b3235a405819a4155a5d2a
Matthias Molitor <mmo@webfactory.de>
2014-10-12 17:43:27 +0200

    implemented iterator (#3)

--- src/Webfactory/Util/DataProviderArgumentIterator.php
+++ src/Webfactory/Util/DataProviderArgumentIterator.php
@@ -9,7 +9,26 @@ namespace Webfactory\Util;
  * A single argument entry is an array that contains the one element for
  * each argument that is passed.
  */
-class DataProviderArgumentIterator
+class DataProviderArgumentIterator extends \IteratorIterator
 {
+    /**
+     * Creates the iterator.
+     *
+     * @param \Traversable|array(mixed) $innerData
+     */
+    public function __construct($innerData)
+    {
+        $innerData = (is_array($innerData)) ? new \ArrayIterator($innerData) : $innerData;
+        parent::__construct($innerData);
+    }
 
+    /**
+     * Converts the original entry into an argument set (with single argument).
+     *
+     * @return array(mixed)
+     */
+    public function current()
+    {
+        return array(parent::current());
+    }
 }
