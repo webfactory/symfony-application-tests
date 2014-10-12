a9c50378bd14c4c638e6044ba4e2508c392de1a0
Matthias Molitor <mmo@webfactory.de>
2014-10-12 17:19:47 +0200

    added comments (#3)

--- tests/Webfactory/Util/DataProviderArgumentIteratorTest.php
+++ tests/Webfactory/Util/DataProviderArgumentIteratorTest.php
@@ -7,16 +7,27 @@ namespace Webfactory\Util;
  */
 class DataProviderArgumentIteratorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Checks if the iterator is traversable.
+     */
     public function testIsTraversable()
     {
 
     }
 
+    /**
+     * Ensures that the iterator does not change the number of entries of the
+     * inner iterator.
+     */
     public function testIteratorDoesNotChangeNumberOfEntries()
     {
 
     }
 
+    /**
+     * Checks if the iterator converts the entries from the inner iterator to
+     * argument sets.
+     */
     public function testIteratorConvertsOriginalEntriesToArgumentSets()
     {
 
