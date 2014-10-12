d4d280fef8a19f6175346772a0fe55c11a874e66
Matthias Molitor <mmo@webfactory.de>
2014-10-12 12:40:40 +0200

    implemented iterator for tagged services (#3)

--- src/Webfactory/Util/TaggedServiceIterator.php
+++ src/Webfactory/Util/TaggedServiceIterator.php
@@ -7,8 +7,22 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 /**
  * Allows iteration over services with a specific tag.
  */
-class TaggedServiceIterator
+class TaggedServiceIterator implements \IteratorAggregate
 {
+    /**
+     * The container that is used to retrieve the service information.
+     *
+     * @var ContainerBuilder
+     */
+    protected $container = null;
+
+    /**
+     * The search tag.
+     *
+     * @var string
+     */
+    protected $tag = null;
+
     /**
      * Creates an iterator that traverses the service IDs with the provided tag.
      *
@@ -17,6 +31,28 @@ class TaggedServiceIterator
      */
     public function __construct(ContainerBuilder $container, $tag)
     {
+        $this->container = $container;
+        $this->tag       = $tag;
+    }
 
+    /**
+     * Returns the iterator.
+     *
+     * @return \Traversable
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     */
+    public function getIterator()
+    {
+        $tagsById       = $this->container->findTaggedServiceIds($this->tag);
+        $taggedServices = array();
+        foreach ($tagsById as $id => $tagDefinitions) {
+            /* @var $id string */
+            /* @var $tagDefinitions array(array(string=>string)) */
+            foreach ($tagDefinitions as $tagDefinition) {
+                /* @var $tagDefinition array(string=>string) */
+                $taggedServices[] = new TaggedService($id, $tagDefinition);
+            }
+        }
+        return new \ArrayIterator($taggedServices);
     }
 }
