23c1ecbadbccd6f1dd2890ee577396ef5f4084bc
Matthias Molitor <mmo@webfactory.de>
2015-02-22 20:22:17 +0100

    defined constructor signature (#11)

--- src/Webfactory/Util/TwigTemplateIterator.php
+++ src/Webfactory/Util/TwigTemplateIterator.php
@@ -2,11 +2,21 @@
 
 namespace Webfactory\Util;
 
+use Symfony\Component\HttpKernel\KernelInterface;
+
 /**
  * Iterator that iterates over the file paths of all Twig templates that belong
  * to the application (application *and* vendor bundles).
  */
 class TwigTemplateIterator
 {
+    /**
+     * Reads templates from the application with the given kernel.
+     *
+     * @param KernelInterface $kernel
+     */
+    public function __construct(KernelInterface $kernel)
+    {
 
+    }
 }
