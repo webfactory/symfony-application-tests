e50568cd9449bee066b1ee979911ca2e3de5ce26
Matthias Molitor <mmo@webfactory.de>
2015-02-22 21:56:50 +0100

    implemented iterator (#11)

--- src/Webfactory/Util/ApplicationFileIterator.php
+++ src/Webfactory/Util/ApplicationFileIterator.php
@@ -5,7 +5,28 @@ namespace Webfactory\Util;
 /**
  * Iterator that filters vendor files and directories out of a given list.
  */
-class ApplicationFileIterator
+class ApplicationFileIterator extends \FilterIterator
 {
+    /**
+     * Creates an iterator that checks the given files.
+     *
+     * @param string[]|\Traversable $files
+     */
+    public function __construct($files)
+    {
+        if (is_array($files)) {
+            $files = new \ArrayIterator($files);
+        }
+        parent::__construct($files);
+    }
 
+    /**
+     * Checks if the current element is accepted.
+     *
+     * @return boolean
+     */
+    public function accept()
+    {
+        return !VendorResources::isVendorFile(parent::current());
+    }
 }
