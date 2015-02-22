f25a13d8a953023929423c111a11ef00cd420969
Matthias Molitor <mmo@webfactory.de>
2015-02-22 20:06:08 +0100

    added test case stub (#11)

---
+++ tests/Webfactory/Util/ApplicationTwigTemplateIteratorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Tests the Twig template iterator.
+ */
+class ApplicationTwigTemplateIteratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsIterator()
+    {
+
+    }
+
+    public function testIteratesOverApplicationLevelTemplates()
+    {
+
+    }
+
+    public function testIteratesOverBundleTemplates()
+    {
+
+    }
+
+    public function testWorksIfNoTemplateFilesAreAvailable()
+    {
+
+    }
+}
