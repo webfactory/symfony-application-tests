4ccf1502c612ef0826551a783e587b7132f3ab72
Matthias Molitor <mmo@webfactory.de>
2015-02-22 21:50:55 +0100

    implemented tests (#11)

--- tests/Webfactory/Util/ApplicationFileIteratorTest.php
+++ tests/Webfactory/Util/ApplicationFileIteratorTest.php
@@ -8,11 +8,41 @@ namespace Webfactory\Util;
 class ApplicationFileIteratorTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Checks if the object is iteratble.
+     * System under test.
+     *
+     * @var ApplicationFileIterator
+     */
+    protected $iterator = null;
+
+    /**
+     * Initializes the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->iterator = new ApplicationFileIterator(array(
+            __FILE__,
+            __DIR__,
+            VendorResources::getVendorDirectory(),
+            VendorResources::getVendorDirectory() . '/autoload.php'
+        ));
+    }
+
+    /**
+     * Cleans up the test environment.
+     */
+    protected function tearDown()
+    {
+        $this->iterator = null;
+        parent::tearDown();
+    }
+
+    /**
+     * Checks if the object is an iterator.
      */
     public function testIsIterator()
     {
-
+        $this->assertInstanceOf('Traversable', $this->iterator);
     }
 
     /**
@@ -20,7 +50,7 @@ class ApplicationFileIteratorTest extends \PHPUnit_Framework_TestCase
      */
     public function testKeepsApplicationFile()
     {
-
+        $this->assertContains(__FILE__, $this->getFiles());
     }
 
     /**
@@ -28,7 +58,7 @@ class ApplicationFileIteratorTest extends \PHPUnit_Framework_TestCase
      */
     public function testKeepsApplicationDirectory()
     {
-
+        $this->assertContains(__DIR__, $this->getFiles());
     }
 
     /**
@@ -36,7 +66,7 @@ class ApplicationFileIteratorTest extends \PHPUnit_Framework_TestCase
      */
     public function testRemovesVendorFile()
     {
-
+        $this->assertNotContains(VendorResources::getVendorDirectory() . '/autoload.php', $this->getFiles());
     }
 
     /**
@@ -44,6 +74,17 @@ class ApplicationFileIteratorTest extends \PHPUnit_Framework_TestCase
      */
     public function testRemovesVendorDirectory()
     {
+        $this->assertNotContains(VendorResources::getVendorDirectory(), $this->getFiles());
+    }
 
+    /**
+     * Returns the files that are provided by the iterator.
+     *
+     * @return string[]
+     */
+    protected function getFiles()
+    {
+        $this->assertInstanceOf('Traversable', $this->iterator);
+        return iterator_to_array($this->iterator);
     }
 }
