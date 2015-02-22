348ad7ca709cffcd8ca25ba77075458c2d1b9ee3
Matthias Molitor <mmo@webfactory.de>
2015-02-22 21:39:58 +0100

    added iterator stub and test case stub (#11)

---
+++ src/Webfactory/Util/ApplicationFileIterator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Iterator that filters vendor files and directories out of a given list.
+ */
+class ApplicationFileIterator
+{
+
+}
---
+++ tests/Webfactory/Util/ApplicationFileIteratorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Tests the application file iterator.
+ */
+class ApplicationFileIteratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Checks if the object is iteratble.
+     */
+    public function testIsIterator()
+    {
+
+    }
+
+    /**
+     * Ensures that the iterator keeps application files.
+     */
+    public function testKeepsApplicationFile()
+    {
+
+    }
+
+    /**
+     * Ensures that the iterator keeps application directories.
+     */
+    public function testKeepsApplicationDirectory()
+    {
+
+    }
+
+    /**
+     * Ensures that the iterator removes vendor files.
+     */
+    public function testRemovesVendorFile()
+    {
+
+    }
+
+    /**
+     * Ensures that the iterator removes vendor directories.
+     */
+    public function testRemovesVendorDirectory()
+    {
+
+    }
+}
