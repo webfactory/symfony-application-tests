da5c36dacffd1f49e7ebbba16fa4ee48696efbbe
Matthias Molitor <mmo@webfactory.de>
2015-02-22 16:23:08 +0100

    added stub for template iterator (#11)

---
+++ src/Webfactory/Util/ApplicationTwigTemplateIterator.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Webfactory\Util;
+
+/**
+ * Iterator that iterates over the file paths of all Twig templates that belong
+ * to the application.
+ */
+class ApplicationTwigTemplateIterator
+{
+
+}
