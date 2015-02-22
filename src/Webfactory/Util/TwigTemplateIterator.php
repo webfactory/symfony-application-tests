2f291cb1567a78dd7e36b847a9e851de7d727164
Matthias Molitor <mmo@webfactory.de>
2015-02-22 21:13:36 +0100

    implemented template iterator (#11)

--- src/Webfactory/Util/TwigTemplateIterator.php
+++ src/Webfactory/Util/TwigTemplateIterator.php
@@ -2,14 +2,24 @@
 
 namespace Webfactory\Util;
 
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * Iterator that iterates over the file paths of all Twig templates that belong
  * to the application (application *and* vendor bundles).
  */
-class TwigTemplateIterator
+class TwigTemplateIterator implements \IteratorAggregate
 {
+    /**
+     * The kernel whose templates are returned.
+     *
+     * @var KernelInterface
+     */
+    protected $kernel = null;
+
     /**
      * Reads templates from the application with the given kernel.
      *
@@ -17,6 +27,39 @@ class TwigTemplateIterator
      */
     public function __construct(KernelInterface $kernel)
     {
-
+        $this->kernel = $kernel;
     }
+
+    /**
+     * Returns an iterator that provides Twig template paths.
+     *
+     * @return \Traversable
+     */
+    public function getIterator()
+    {
+        $this->kernel->boot();
+        $viewDirectories = array();
+        $globalResourceDirectory = $this->kernel->getRootDir() . '/Resources';
+        if (is_dir($globalResourceDirectory)) {
+            $viewDirectories[] = $globalResourceDirectory;
+        }
+        foreach ($this->kernel->getBundles() as $bundle) {
+            /* @var $bundle BundleInterface */
+            $viewDirectory = $bundle->getPath() . '/Resources/views';
+            if (is_dir($viewDirectory)) {
+                $viewDirectories[] = $viewDirectory;
+            }
+        }
+        if (count($viewDirectories) === 0) {
+            return new \ArrayIterator();
+        }
+        $templates = Finder::create()->in($viewDirectories)->files()->name('*.*.twig');
+        $templates = iterator_to_array($templates, false);
+        $templates = array_map(function (SplFileInfo $file) {
+            return $file->getRealPath();
+        }, $templates);
+        return new \ArrayIterator($templates);
+    }
+
+
 }
