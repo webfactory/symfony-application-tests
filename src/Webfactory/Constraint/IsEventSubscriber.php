513cf4aa8e2d26f8fd079858ff2e4c17d909f0d7
Matthias Molitor <mmo@webfactory.de>
2015-03-13 16:06:17 +0100

    partially implemented subscriber constraint (#14)

--- src/Webfactory/Constraint/IsEventSubscriber.php
+++ src/Webfactory/Constraint/IsEventSubscriber.php
@@ -2,11 +2,20 @@
 
 namespace Webfactory\Constraint;
 
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
 /**
  * Checks if an object is a correctly configured Symfony event subscriber.
  */
 class IsEventSubscriber extends \PHPUnit_Framework_Constraint
 {
+    /**
+     * List of detected problems.
+     *
+     * @var string[]
+     */
+    private $detectedProblems = array();
+
     /**
      * Checks if the given object is an event subscriber.
      *
@@ -15,7 +24,22 @@ class IsEventSubscriber extends \PHPUnit_Framework_Constraint
      */
     protected function matches($other)
     {
-        return parent::matches($other);
+        $this->resetProblems();
+        if (!($other instanceof EventSubscriberInterface)) {
+            $this->addProblem('Subscriber must implement \Symfony\Component\EventDispatcher\EventSubscriberInterface.');
+            return false;
+        }
+        $subscribedEvents = call_user_func(array(get_class($other), 'getSubscribedEvents'));
+        if (!is_array($subscribedEvents)) {
+            $this->addProblem('getSubscribedEvents() must return an array.');
+            return false;
+        }
+        foreach ($subscribedEvents as $event => $subscription) {
+            /* @var $event string */
+            /* @var $subscription mixed|string|array */
+            $this->checkListener($event, $subscription);
+        }
+        return !$this->hasProblems();
     }
 
     /**
@@ -25,6 +49,70 @@ class IsEventSubscriber extends \PHPUnit_Framework_Constraint
      */
     public function toString()
     {
-        // TODO: Implement toString() method.
+        return 'is a valid EventSubscriber.';
+    }
+
+    /**
+     * Checks if the listener definition for the given event is valid.
+     *
+     * @param string $event
+     * @param mixed $listener
+     */
+    protected function checkListener($event, $listener)
+    {
+        if (is_string($listener)) {
+            // Add the default priority and use the default validation.
+            $listener = array($listener, 0);
+        }
+        if (!is_array($listener)) {
+            $this->addProblem(sprintf('Listener definition for event "%s" must be an array or a string.', $event));
+            return;
+        }
+
+    }
+
+    /**
+     * Adds a problem to the list.
+     *
+     * @param string $description
+     */
+    protected function addProblem($description)
+    {
+        $this->detectedProblems[] = $description;
+    }
+
+    /**
+     * Checks if problems have been detected.
+     *
+     * @return boolean
+     */
+    protected function hasProblems()
+    {
+        return count($this->detectedProblems) > 0;
+    }
+
+    /**
+     * Resets the list of problems.
+     */
+    protected function resetProblems()
+    {
+        $this->detectedProblems = array();
+    }
+
+    /**
+     * Return additional failure description where needed
+     *
+     * The function can be overridden to provide additional failure
+     * information like a diff
+     *
+     * @param  mixed  $other Evaluated value or object.
+     * @return string
+     */
+    protected function additionalFailureDescription($other)
+    {
+        $problems = array_map(function ($problem) {
+            return '- ' . $problem;
+        }, $this->detectedProblems);
+        return implode(PHP_EOL, $problems);
     }
 }
