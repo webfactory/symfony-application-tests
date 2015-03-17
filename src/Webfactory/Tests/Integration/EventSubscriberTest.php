df11b1e46677ea61d9458b3f4fdbd64ff5985dca
Matthias Molitor <mmo@webfactory.de>
2015-03-17 09:19:39 +0100

    added test case that checks event subscribers (#14)

---
+++ src/Webfactory/Tests/Integration/EventSubscriberTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Webfactory\Tests\Integration;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Webfactory\Constraint\IsEventSubscriber;
+use Webfactory\Util\ServiceCreator;
+use Webfactory\Util\TaggedService;
+
+/**
+ * Checks registered event subscribers.
+ */
+class EventSubscriberTest extends AbstractContainerTestCase
+{
+    /**
+     * Checks if registered event subscribers use correct method references.
+     *
+     * @param TaggedService $service
+     * @dataProvider getEventSubscribers
+     */
+    public function testRegisteredEventSubscribers(TaggedService $service = null)
+    {
+        if ($service === null) {
+            $this->markTestSkipped('No event subscribers registered, nothing to test.');
+        }
+
+        $creator = new ServiceCreator($this->getContainer());
+        /* @var $subscriber EventSubscriberInterface */
+        $subscriber = $creator->create($service->getServiceId());
+        $this->assertThat($subscriber, new IsEventSubscriber());
+    }
+
+    /**
+     * Returns the registered event subscribers.
+     *
+     * @return \Traversable
+     */
+    public function getEventSubscribers()
+    {
+        return $this->getTaggedServices('kernel.event_subscriber');
+    }
+}
